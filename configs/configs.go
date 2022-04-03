cb33969a767f74debfafe852683ee2a717b93f25
zarif-al <zarif_al96@outlook.com>
2022-04-03 09:36:26 +0600

    scaffolding-update

---
+++ configs/configs.go
@@ -0,0 +1,53 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func EnvMongoURI() string {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	return os.Getenv("MONGO_URI")
+}
+
+func ConnectDB() *mongo.Client {
+	// Set up Client
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoURI()))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Set up context with timeout for connection
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+
+	// Attempt Connection
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ping DB
+	err = client.Ping(ctx, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB")
+
+	return client
+}
